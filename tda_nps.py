93835be2ab996d207376964c39ef04dd75867c3c
Neil Strickland <N.P.Strickland@sheffield.ac.uk>
2024-11-19 10:59:45 +0000

    Initial commit

---
+++ tda_nps.py
@@ -0,0 +1,10 @@
+import numpy as np
+import gtda
+import matplotlib.pyplot as plt
+from gtda.plotting import plot_diagram
+from space import *
+
+X = ComplexProjectiveSpace(2)
+print(X.name)
+print(X.dim)
+print(X.edim)
